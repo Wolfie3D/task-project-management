f9e5f380ba52e4f424da45855518273144c41bc9
Chris Kamau <kwangechi3@gmail.com>
2024-06-05 12:28:17 -0700

    Models-Updated the relations in the models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['status', 'created_by', 'due_date', 'description', 'title'];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+
 }
